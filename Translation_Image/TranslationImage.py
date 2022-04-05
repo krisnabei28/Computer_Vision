a8bd071cf9fe2447c702bbc8e57b1997026c62a4
krisnabei28 <krisnaabei28@gmail.com>
2022-04-05 16:21:40 +0700

    Translation Image

---
+++ Translation_Image/TranslationImage.py
@@ -0,0 +1,19 @@
+
+import cv2 as cv
+import numpy as np
+  
+image = cv.imread('/home/krisnabei/Documents/Dokumen_Krisna/DeepLearning/Code/ImageDataSet/Picture06.jpg')
+
+height, width = image.shape[:2]
+  
+quarter_height, quarter_width = height / 4, width / 4
+  
+T = np.float32([[1, 0, quarter_width], [0, 1, quarter_height]])
+
+img_translation = cv.warpAffine(image, T, (width, height))
+  
+cv.imshow("Original image", image)
+cv.imshow('Translation image', img_translation)
+cv.waitKey()
+  
+cv.destroyAllWindows()
